e1542c8f6c40081f811ae7272cdf9d3e89b86ffb
barvihabarviha44-coder <barvihabarviha44@gmail.com>
2026-03-29 12:21:05 +0400

    Update knb.py

--- handlers/games/knb.py
+++ handlers/games/knb.py
@@ -69,4 +69,51 @@ async def knb_callback(callback: CallbackQuery):
     choice_emoji = {'rock': '🪨', 'scissors': '✂️', 'paper': '📄'}
     choice_name = {'rock': 'Камень', 'scissors': 'Ножницы', 'paper': 'Бумага'}
     
-    #
+        # Определяем победителя
+    if player_choice == bot_choice:
+        # Ничья - возврат ставки
+        await db.update_balance(callback.from_user.id, bet, add=True)
+        await db.update_task_progress(callback.from_user.id, 'knb_play')
+        
+        text = f"""
+🤝 <b>Ничья!</b>
+
+Вы: {choice_emoji[player_choice]} {choice_name[player_choice]}
+Бот: {choice_emoji[bot_choice]} {choice_name[bot_choice]}
+
+{EMOJI['coin']} Ставка возвращена: <b>{format_number(bet)} VC</b>
+"""
+    elif (player_choice == 'rock' and bot_choice == 'scissors') or \
+         (player_choice == 'scissors' and bot_choice == 'paper') or \
+         (player_choice == 'paper' and bot_choice == 'rock'):
+        # Победа
+        winnings = bet * 2
+        await db.update_balance(callback.from_user.id, winnings, add=True)
+        await db.update_stats(callback.from_user.id, won=winnings, played=1, game_won=True)
+        await db.update_task_progress(callback.from_user.id, 'knb_play')
+        
+        text = f"""
+{EMOJI['check']} <b>Победа!</b>
+
+Вы: {choice_emoji[player_choice]} {choice_name[player_choice]}
+Бот: {choice_emoji[bot_choice]} {choice_name[bot_choice]}
+
+{EMOJI['coin']} Выигрыш: <b>{format_number(winnings)} VC</b> (x2)
+"""
+    else:
+        # Проигрыш
+        await db.update_stats(callback.from_user.id, lost=bet, played=1)
+        await db.add_to_jackpot(bet)
+        await db.update_task_progress(callback.from_user.id, 'knb_play')
+        
+        text = f"""
+{EMOJI['cross']} <b>Проигрыш!</b>
+
+Вы: {choice_emoji[player_choice]} {choice_name[player_choice]}
+Бот: {choice_emoji[bot_choice]} {choice_name[bot_choice]}
+
+{EMOJI['coin']} Потеря: <b>{format_number(bet)} VC</b>
+"""
+    
+    await maybe_add_xp(callback.from_user.id)
+    await callback.message.edit_text(text, reply_markup=get_back_button("menu_games"), parse_mode="HTML")
